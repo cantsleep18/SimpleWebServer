f6e1327cea49ad4b041b92322459fc16f310c7d4
cantsleep <cant_sleep@naver.com>
2022-10-04 16:43:27 +0900

    WebServer With ServeMux

---
+++ web_server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func barHandler(w http.ResponseWriter, r *http.Request) {
+	values := r.URL.Query()
+	name := values.Get("name")
+	if name == "" {
+		name = "World"
+	}
+	id, _ := strconv.Atoi(values.Get("id"))
+	fmt.Fprintf(w, "Hello %s! id:%d", name, id)
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World")
+	})
+
+	mux.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request)  {
+		fmt.Fprintf(w, "Hello Bar")
+	})
+	http.ListenAndServe(":3000", mux)
+}
